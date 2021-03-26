83d6db063780beff61e299fea099cd9f683c3b35
zxyue25 <zxy@syzc.net.cn>
2021-03-26 17:35:31 +0800

    feat:bus

--- bus.js
+++ bus.js
@@ -1,12 +1,10 @@
-const event = require('event-helper')
-
+import event from 'event-helper'
 const globalEventPrefix = 'jdt-widget-'
-
-export default class{
-  constructor(namespace){
-    const eventNamespace =  globalEventPrefix + namespace
-    if(!window[eventNamespace]){
-      window[eventNamespace] = new event()
+export default class eventCanter {
+  constructor(namespace) {
+    const eventNamespace = globalEventPrefix + namespace
+    if (!window[eventNamespace]) {
+      window[eventNamespace] = event.create()
     }
     return window[eventNamespace]
   }
